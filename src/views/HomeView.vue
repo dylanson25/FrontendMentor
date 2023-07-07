8eda6b95613c586d0f9aaf635b239a88e6d3bc2d
Dylanson25 <dvillarreal0@ucol.mx>
2023-07-06 19:45:03 -0600

    initial config

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main class="main">
+    <Button />
+    <Button expanded />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { Button } from "@/components";
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    Button,
   },
 };
 </script>
+<style lang="sass" scoped>
+.main
+  display: flex
+  justify-content: center
+  align-items: center
+  min-height: 100vh
+  width: 100%
+</style>
