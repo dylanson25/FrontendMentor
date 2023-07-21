7191fdddc9cd2bd7411f466c901da258f53606c4
Dylanson25 <dvillarreal0@ucol.mx>
2023-07-20 21:42:08 -0600

    router config

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,68 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    redirect: "/mercury",
+  },
+  {
+    path: "/mercury",
+    name: "Mercury",
+    component: () =>
+      import(/* webpackChunkName: "Mercury" */ "../views/HomeView.vue"),
+  },
+  {
+    path: "/venus",
+    name: "Venus",
+    component: () =>
+      import(/* webpackChunkName: "Venus" */ "../views/HomeView.vue"),
+  },
+  {
+    path: "/earth",
+    name: "Earth",
+    component: () =>
+      import(/* webpackChunkName: "Earth" */ "../views/HomeView.vue"),
+  },
+  {
+    path: "/mars",
+    name: "Mars",
+    component: () =>
+      import(/* webpackChunkName: "Mars" */ "../views/HomeView.vue"),
+  },
+  {
+    path: "/jupiter",
+    name: "Jupiter",
+    component: () =>
+      import(/* webpackChunkName: "Jupiter" */ "../views/HomeView.vue"),
+  },
+  {
+    path: "/saturn",
+    name: "Saturn",
+    component: () =>
+      import(/* webpackChunkName: "Saturn" */ "../views/HomeView.vue"),
+  },
+  {
+    path: "/uranus",
+    name: "Uranus",
+    component: () =>
+      import(/* webpackChunkName: "Uranus" */ "../views/HomeView.vue"),
+  },
+  {
+    path: "/neptune",
+    name: "Neptune",
+    component: () =>
+      import(/* webpackChunkName: "Neptune" */ "../views/HomeView.vue"),
   },
 ];
-// import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  document.title = to.name;
+  if (to.path === "/") next("/mercury");
+  else next();
 });
 
 export default router;
